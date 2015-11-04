020ad016113b4abdd12a4db76fd1196db4aa4d28
Oleh Belkot <o.belkot@gmail.com>
2015-11-04 21:49:38 +0200

    Add validation for user name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :name, length: { in: 3..20 }
 
   before_save { self.admin = true unless User.any? }
 
