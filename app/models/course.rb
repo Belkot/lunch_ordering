7ce5c488d85589bf4ca74f7808aa49eb37edeaaa
Oleh Belkot <o.belkot@gmail.com>
2015-11-05 20:59:31 +0200

    Add callbacks for update and delete courses.

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,25 @@ class Course < ActiveRecord::Base
   belongs_to :course_type, inverse_of: :courses
   has_many :line_items
   has_many :orders, through: :line_items
+
+  before_update :updater
+  before_destroy :deleter
+  after_rollback :saver_self
+
+  private
+
+    def updater
+      Course.create( name: name, price: price, course_type_id: course_type_id )
+      self.reload
+      self.deleted_at = DateTime.now
+    end
+
+    def deleter
+      false
+    end
+
+    def saver_self
+      self.deleted_at = DateTime.now
+      self.save
+    end
 end
