930ea1c365012e4c11d6819f908dd455f9ccd012
Oleh Belkot <o.belkot@gmail.com>
2015-11-05 16:51:51 +0200

    Add callback to ensure when add items only form one type to one order.

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,17 @@
 class LineItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :course
+
+  before_save :ensure_one_course_for_each_course_type_in_one_order
+
+  private
+    def ensure_one_course_for_each_course_type_in_one_order()
+      if self.order.courses.exists?( course_type_id: self.course.course_type.id )
+        self.errors.add :base, 'Only one course from each course types'
+        return false
+      else
+        return true
+      end
+    end
+
 end
