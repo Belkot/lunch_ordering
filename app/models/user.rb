18f6f037143f70515ee5ce423babacfa6e2639b3
Oleh Belkot <o.belkot@gmail.com>
2015-11-06 14:29:45 +0200

    Add avatar to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  mount_uploader :avatar, AvatarUploader
+
   has_many :orders, inverse_of: :user
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -7,5 +9,9 @@ class User < ActiveRecord::Base
   validates :name, length: { in: 3..20 }
 
   before_save { self.admin = true unless User.any? }
+
+  # validates_presence_of   :avatar
+  # validates_integrity_of  :avatar
+  # validates_processing_of :avatar
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105124819) do
+ActiveRecord::Schema.define(version: 20151106114003) do
 
   create_table "course_types", force: :cascade do |t|
     t.string   "name"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20151105124819) do
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
     t.string   "name"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
