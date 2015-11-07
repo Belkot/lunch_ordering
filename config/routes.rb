a5b577bd74f2fb989fccaebd96871ddb99da4523
Oleh Belkot <o.belkot@gmail.com>
2015-11-07 13:44:00 +0200

    Change routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'user/index'
-
-  get 'dashboard/index'
+  get 'users', to: 'users#index'
+  get 'dashboard', to: 'dashboard#index', as: 'dashboard'
 
   devise_for :users
-
-  root 'dashboard#index'
 
   resources :course_types, except: [:show, :destroy]
   resources :courses
 
+  root 'dashboard#index'
 end
